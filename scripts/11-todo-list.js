7e0806519b8e51fcdf49592f9d44b311868c0fc3
ohendarko <ohendarko@gmail.com>
2024-03-19 20:41:03 +0000

    styling delete buttons

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -30,7 +30,7 @@ function renderTodoList() {
       <button onclick="
         todoList.splice(${i}, 1);
         renderTodoList();
-      ">Delete</button>
+      " class="delete-todo-button">Delete</button>
     `;
     todoListHTML += html;
   }
