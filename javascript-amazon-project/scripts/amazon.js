4dade24c62e77b08962e2f333bd92d150890ba53
ohendarko <ohendarko@gmail.com>
2024-04-14 19:20:51 +0000

    made cart quantity responsive

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -83,6 +83,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       cartQuantity += item.quantity;
     });
 
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     console.log(cartQuantity);
     console.log(cart);
   });
