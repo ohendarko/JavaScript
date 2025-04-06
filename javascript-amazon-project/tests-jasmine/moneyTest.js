56e04b7dda2726f9d47005c1fcc24a09d7a47bb7
ohendarko <ohendarko@gmail.com>
2025-04-06 21:49:09 +0000

    translating previous logic to jasmine with expect and toEqual

--- javascript-amazon-project/tests-jasmine/moneyTest.js
+++ javascript-amazon-project/tests-jasmine/moneyTest.js
@@ -1,3 +1,7 @@
 import { formatCurrency } from '../scripts/utils/money.js';
 
-describe()
+describe('test suite: formatCurrency', () => {
+  it('converts cents into dollars', () => {
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+});
