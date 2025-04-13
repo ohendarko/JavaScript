2960e1d137cd7a8de5b6fcaa97204adfd34cbbfd
ohendarko <ohendarko@gmail.com>
2025-04-13 19:17:48 +0000

    flaky tests and mocking

--- javascript-amazon-project/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project/tests-jasmine/data/cartTest.js
@@ -6,6 +6,10 @@ describe('test suite: addToCart', () => {
   });
 
   it('adds a new product to the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      
+    });
+
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length.toEqual(1));
   });
