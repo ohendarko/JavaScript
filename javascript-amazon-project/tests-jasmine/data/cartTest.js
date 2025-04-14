94371b997479d56347b406680f93e08fbd9574ca
ohendarko <ohendarko@gmail.com>
2025-04-14 11:09:16 +0000

    we didnt have to add a function for this one. toHaveBeenCalledTimes checks how many times the function is called

--- javascript-amazon-project/tests-jasmine/data/cartTest.js
+++ javascript-amazon-project/tests-jasmine/data/cartTest.js
@@ -6,9 +6,7 @@ describe('test suite: addToCart', () => {
   });
 
   it('adds a new product to the cart', () => {
-    spyOn(localStorage, 'setItem').and.callFake(() => {
-
-    });
+    spyOn(localStorage, 'setItem');
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
@@ -18,5 +16,6 @@ describe('test suite: addToCart', () => {
 
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
   });
 });
