1aaf483c80937b64b736befa227e4bf8b870a080
ohendarko <ohendarko@gmail.com>
2025-04-16 00:10:24 +0000

    adding save to storage to object

--- javascript-amazon-project/data/cart-oop.js
+++ javascript-amazon-project/data/cart-oop.js
@@ -17,7 +17,9 @@ const cart = {
       ];
   },
 
-  
+  saveToStorage() {
+    localStorage.setItem('cart-oop', JSON.stringify(this.cartItems));
+  }
 
 };
 
@@ -30,27 +32,7 @@ loadFromStorage();
 
 
 
-export function addToCart(productId) {
-  let matchingItem;
 
-  cart.forEach((cartItem) => {
-    if (productId === cartItem.productId) {
-      matchingItem = cartItem;
-    }
-  });
-
-  if (matchingItem) {
-    matchingItem.quantity += 1;
-  } else {
-    cart.push({
-      productId: productId,
-      quantity: 1,
-      deliveryOptionId: '1'
-    });
-  }
-
-  saveToStorage();
-}
 
 export function removeFromCart(productId) {
   const newCart = [];
