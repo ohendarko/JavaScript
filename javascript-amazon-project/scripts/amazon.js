3bd98426f7573b766fd89cdc90c76a9a9b2c011c
ohendarko <ohendarko@gmail.com>
2025-04-29 23:22:15 +0000

    removing unused import

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,6 +1,6 @@
 import { cart, addToCart } from "../data/cart.js";
 import { products, loadProducts } from "../data/products.js";
-//import { formatCurrency } from "./utils/money.js";
+
 
 loadProducts(renderProductsGrid);
 
